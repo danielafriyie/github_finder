f679e94c7d02a1a2ff8503ebb48b317b849c44bf
danielafriyie <afriyiedaniel1@outlook.com>
2021-01-01 09:59:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# GITHUB FINDER
+Single page website used to find github users. You search for the users<br>
+username then it'll fetch information about the user with his latest five <br>
+repositories.
+
+![!](img/home.png "")<br><br>
+![!](img/main.png "")
+
+## Installation
+- Clone this repo. `git clone <repo-url>`
+- Open `index.html`
+- Search for any github user.
+
+## Author
+
+* **Afriyie Daniel**
+
+Hope You Enjoy Using It !!!!
---
+++ github.js
@@ -0,0 +1,28 @@
+//  e5597f9800c4354121be350a4f69c501f08d6df7
+
+class  GitHub {
+    constructor() {
+        this.client_id = '8f35bdd246a2bb16a6ad';
+        this.client_secret = 'e5597f9800c4354121be350a4f69c501f08d6df7';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(
+            `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+
+        const reposResponse = await fetch(
+            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&
+                   client_id=${this.client_id}&client_secret=${this.client_secret}`
+        );
+
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+        return {
+            profile: profile,
+            repos: repos
+        }
+    }
+}
